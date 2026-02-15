ea08819a9f4383b12cff890dd1ce2f8c2177864c
PUTIJIRAF\llucs <llucsegala29@gmail.com>
2026-02-15 01:39:44 +0100

    inflexion 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // Aquesta és la línia que fallava
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
